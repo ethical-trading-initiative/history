531fcf3eaf274f4da7b2d2e954d57ad2d67df2a2
Dan Bohea <git@bohea.co.uk>
2017-12-07 10:23:13 +0000

    JS refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,29 @@
 jQuery(document).ready(function($){
-	var timelineBlocks = $('.timeline-entry');
+
+	var timelineEntries = $('.timeline-entry');
   // TODO: offset seems very sensitive to content (header height?)
 	var offset = 0.9; // was 0.8
 
-	//hide timeline blocks which are outside the viewport
-	hideBlocks(timelineBlocks, offset);
+	// Hide timeline entries that are outside the viewport.
+	hideEntries(timelineEntries, offset);
 
-	//on scolling, show/animate timeline blocks when enter the viewport
+	// On scolling, show/animate timeline entries when they enter the viewport.
 	$(window).on('scroll', function(){
 		(!window.requestAnimationFrame)
-			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
-			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+			? setTimeout(function(){ showEntries(timelineEntries, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showEntries(timelineEntries, offset); });
 	});
 
-	function hideBlocks(blocks, offset) {
-		blocks.each(function(){
+	function hideEntries(entries, offset) {
+		entries.each(function(){
 			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.timeline-entry-icon, .timeline-entry-content').addClass('is-hidden');
 		});
 	}
 
-	function showBlocks(blocks, offset) {
-		blocks.each(function(){
+	function showEntries(entries, offset) {
+		entries.each(function(){
 			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.timeline-entry-icon').hasClass('is-hidden') ) && $(this).find('.timeline-entry-icon, .timeline-entry-content').removeClass('is-hidden').addClass('bounce-in');
 		});
 	}
+
 });
