5574fceef06f8525c1663455994318af8801bc39
Dan Bohea <git@bohea.co.uk>
2017-12-12 10:10:17 +0000

    Tidying up

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,43 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
 
-  // Timeline
+  // Reveal timeline entires on scroll
   // ---------------------------------------------------------------------------
 
-	var timelineEntries = $('.timeline-entry');
+  var timelineEntries = $('.timeline-entry');
   // TODO: offset seems very sensitive to content (header height?)
-	var offset = 0.9; // was 0.8
+  var offset = 0.9; // was 0.8
 
-	// Hide timeline entries that are outside the viewport.
-	hideEntries(timelineEntries, offset);
+  // Hide timeline entries that are outside the viewport.
+  hideEntries(timelineEntries, offset);
 
-	// On scolling, show/animate timeline entries when they enter the viewport.
-	$(window).on('scroll', function(){
-		(!window.requestAnimationFrame)
-			? setTimeout(function(){ showEntries(timelineEntries, offset); }, 100)
-			: window.requestAnimationFrame(function(){ showEntries(timelineEntries, offset); });
-	});
+  // On scolling, show/animate timeline entries when they enter the viewport.
+  $(window).on('scroll', function() {
+    (!window.requestAnimationFrame) ?
+    setTimeout(function() {
+      showEntries(timelineEntries, offset);
+    }, 100): window.requestAnimationFrame(function() {
+      showEntries(timelineEntries, offset);
+    });
+  });
 
-	function hideEntries(entries, offset) {
-		entries.each(function(){
-			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.timeline-entry-date, .timeline-entry-content').addClass('is-hidden');
-		});
-	}
+  function hideEntries(entries, offset) {
+    entries.each(function() {
+      ($(this).offset().top > $(window).scrollTop() + $(window).height() * offset) && $(this).find('.timeline-entry-date, .timeline-entry-content').addClass('is-hidden');
+    });
+  }
 
-	function showEntries(entries, offset) {
-		entries.each(function(){
-			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.timeline-entry-date').hasClass('is-hidden') ) && $(this).find('.timeline-entry-date, .timeline-entry-content').removeClass('is-hidden').addClass('bounce-in');
-		});
-	}
+  function showEntries(entries, offset) {
+    entries.each(function() {
+      ($(this).offset().top <= $(window).scrollTop() + $(window).height() * offset && $(this).find('.timeline-entry-date').hasClass('is-hidden')) && $(this).find('.timeline-entry-date, .timeline-entry-content').removeClass('is-hidden').addClass('bounce-in');
+    });
+  }
 
   // Image & video galleries
   // ---------------------------------------------------------------------------
 
   $("[data-fancybox]").fancybox({
-	   loop : true,
-     buttons : ['close']
+    loop: true,
+    buttons: ['close']
   });
 
 });
